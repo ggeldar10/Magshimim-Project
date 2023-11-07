bebc8c86d6c4e67be2c33e7da1fb9b4e57b9a26e
Eldar Timor <timoreldar@gmail.com>
2023-11-07 18:08:18 +0200

    implemented the function bind

--- TeamViewer/UdpSocket.cpp
+++ TeamViewer/UdpSocket.cpp
@@ -3,6 +3,10 @@
 UdpSocket::UdpSocket() : _socket(NULL), _port(0)
 {
 	this->_socket = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+	if (this->_socket < 0)
+	{
+		throw "Error creating the socket";
+	}
 	this->_port = 0;
 }
 
@@ -12,7 +16,12 @@ UdpSocket::~UdpSocket()
 	this->_socket = NULL;
 }
 
-int UdpSocket::sendTo(std::string msg, sockaddr* to)
+/*
+this function will send the msg to the given address
+input: the message to send, the address to send to
+output: the error code of sendto
+*/
+int UdpSocket::sendTo(std::string msg, sockaddr* to) // maybe change later to a real exception insted of retruning the error code
 {
 	if (this->_socket == NULL)
 	{
@@ -21,6 +30,11 @@ int UdpSocket::sendTo(std::string msg, sockaddr* to)
 	return sendto(this->_socket, msg.c_str(), msg.size(), 0, to, sizeof(*to));
 }
 
+/*
+this function will send the msg to the given address
+input: the message to send, the address to send to
+output: the error code of sendto
+*/
 int UdpSocket::sendTo(char* msg, sockaddr* to)
 {
 	if (this->_socket == NULL)
@@ -35,7 +49,18 @@ int UdpSocket::recv(std::string buffer, int len)
 	return 0;
 }
 
-void UdpSocket::bind(int port, sockaddr* addrs)
+/*
+binds the the socket to listen to incoming messages 
+using a certain port
+input: the port to listen to, the local address
+output: the socket is listening to incoming messages
+*/
+void UdpSocket::bindUdpSocket(int port, sockaddr* addrs)
 {
-
+	if (this->_socket == NULL)
+	{
+		throw "Error: socket is null"
+	}
+	bind(this->_socket, addrs, sizeof(*addrs));
+	this->_port = port;
 }
