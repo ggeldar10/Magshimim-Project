77f538f05c816ed632b76630d80e3cfc8e292d91
Eldar Timor <timoreldar@gmail.com>
2023-11-14 10:56:30 +0200

    checked the gui of the controller window and made the controlled window

--- teamViewerGui/teamViewerGui/ControlledWindow.xaml.cs
+++ teamViewerGui/teamViewerGui/ControlledWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace teamViewerGui
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ControlledWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ControlledWindow : Window
     {
-        public MainWindow()
+        public ControlledWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            ControllerWindow window = new ControllerWindow();
-            window.Show();
         }
     }
 }
