539edc84e66c34c60d5ee42b513517cdb93d763d
Eldar Timor <timoreldar@gmail.com>
2024-03-16 12:30:52 +0000

    made a class to capture the screen

---
+++ TeamViewer/ImageCapture.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <windows.h>
+#include <gdiplus.h>
+#include <iostream>
+
+#pragma comment(lib,"gdiplus.lib")
+
+using namespace Gdiplus;
+
+class ImageCapture
+{
+private:
+	HDC _hdcScreen;
+	HDC _screenshotMem;
+	unsigned int _width;
+	unsigned int _height;
+	HBITMAP _hbitmap;
+	GdiplusStartupInput _gdip;
+	ULONG_PTR _gdipToken;
+
+public:
+	ImageCapture(HWND hWindow);
+	~ImageCapture();
+	std::unique_ptr<Bitmap> captureScreen();
+
+	// A function from msdn to get the clsid
+	// format can be:
+	// image/png, image/jpeg, image/bmp
+	static int getEncoderClsid(const WCHAR* format, CLSID* pClsid);
+
+};
+
